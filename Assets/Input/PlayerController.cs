c724956c225f1285f8b7f1d3629a26e750f1ea5c
AlexandreSanchesMateus <90258109+AlexandreSanchesMateus@users.noreply.github.com>
2022-10-11 14:40:08 +0200

    PlayerContoller with Rigidbody

--- Assets/Input/PlayerController.cs
+++ Assets/Input/PlayerController.cs
@@ -3,48 +3,77 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
     private PlayerInput playerInput;
+
     private InputAction movement;
+    private InputAction looking;
 
+    private Rigidbody rb;
+    private float xRotation = 0.0f;
 
+    [SerializeField] private GameObject cameraObj;
+    [SerializeField] private float speed = 100.0f;
+    [SerializeField] private float sensitivity = 100.0f;
+    
+    
     private void Awake()
     {
         playerInput = new PlayerInput();
     }
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
     private void OnEnable()
     {
-        playerInput.FPSController.Move.performed += DoMove;
-        playerInput.FPSController.Move.Enable();
-        
-        playerInput.FPSController.Look.performed += DoLook;
-        playerInput.FPSController.Look.Enable();
-       // movement = playerInput.FPSController.Move;
-       // movement.Enable();
+        movement = playerInput.FPSController.Move;
+        movement.Enable();
+
+        looking = playerInput.FPSController.Look;
+        looking.Enable();
 
         playerInput.FPSController.Interact.performed += DoInteract;
         playerInput.FPSController.Interact.Enable();
 
     }
+
+    private void OnDisable()
+    {
+        movement.Disable();
+        looking.Disable();
+        playerInput.FPSController.Interact.Disable();
+    }
+
     private void DoInteract(InputAction.CallbackContext ctx)
     {
         Debug.Log("interact");
     }
 
-    private void DoMove(InputAction.CallbackContext ctx)
+    private void Update()
     {
-        Debug.Log("Movement Vector 2 : " + ctx.ReadValue<Vector2>());
-    }
-    
-    private void DoLook(InputAction.CallbackContext ctx)
-    {
-        Debug.Log("Looking Vector 2 : " + ctx.ReadValue<Vector2>());
+        // Souris horizontale
+        transform.Rotate(Vector3.up * (looking.ReadValue<Vector2>().x * sensitivity * Time.deltaTime));
+
+        // Souris verticale
+        xRotation -= looking.ReadValue<Vector2>().y * sensitivity * Time.deltaTime;
+        xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
+        cameraObj.transform.localRotation = Quaternion.Euler(xRotation, 0.0f, 0.0f);
     }
 
     private void FixedUpdate()
     {
-        
+        // Déplacement dans le monde
+        Debug.Log("Movement : " + movement.ReadValue<Vector2>());
+        Vector3 direction = transform.rotation * new Vector3(movement.ReadValue<Vector2>().x, 0.0f, movement.ReadValue<Vector2>().y);
+
+        rb.AddForce(direction * speed * Time.fixedDeltaTime);
     }
 }
 
