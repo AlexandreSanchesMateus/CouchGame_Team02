e2ee24015bbed9e691a730607d522ccfefd23518
Cryp3rone <Manach.Nicolas.mn@gmail.com>
2022-10-14 11:32:03 +0200

    Timer

--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -7,33 +7,65 @@ using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    public float timeValue = 90f;
-    public TextMeshProUGUI timerText;
+	public float timeRemaining, timeValue = 90f;
+	public TextMeshProUGUI timerText;
+	public GameObject gameOverScreen;
 
+	private bool timerIsRunning = false;
+
+    private void Start()
+    {
+		timeRemaining = timeValue;
+		timerIsRunning = true;
+    }
     // Update is called once per frame
     void Update()
+	{
+        if (timerIsRunning)
+        {
+			if (timeRemaining > 0)
+			{
+				timeRemaining -= Time.deltaTime;
+
+				DisplayTime(timeRemaining);
+			}
+			else
+			{
+
+				timeRemaining = 0;
+				timerIsRunning = false;
+				DisplayGameOver();
+			}
+        }
+	}
+
+	public void DisplayTime(float timeToDisplay)
+	{
+		if (timeToDisplay < 0)
+		{
+			timeToDisplay = 0;
+		}
+		//timeToDisplay += 1;
+		float minutes = timeToDisplay / 60;
+		float secondes = timeToDisplay % 60;
+		float milliseconds = (timeToDisplay % 1) * 1000;
+
+		timerText.text = string.Format("{0:00} : {1:00} : {2:000}", minutes, secondes, milliseconds);
+	}
+
+	public void DisplayGameOver()
     {
-        if (timeValue > 0)
-        {
-            timeValue -= Time.deltaTime;
-        }
-        else
-        {
-            timeValue = 0;
-        }
+		Time.timeScale = 0;
+		gameOverScreen.SetActive(true);
+		Cursor.lockState = CursorLockMode.Confined;
+	}
 
-        DisplayTime(timeValue);
-    }
-
-    public void DisplayTime(float timeToDisplay)
+	public void DisplayGame()
     {
-        if (timeToDisplay < 0)
-        {
-            timeToDisplay = 0;
-        }
-        float minutes = timeToDisplay / 60;
-        float secondes = timeToDisplay % 60;
-
-        timerText.text = string.Format("{0:00} : {1:00}", minutes, secondes);
-    }
+		Time.timeScale = 1;
+		timeRemaining = timeValue;
+		gameOverScreen.SetActive(false);
+		timerIsRunning = true;
+		Cursor.lockState = CursorLockMode.Locked;
+	}
 }
