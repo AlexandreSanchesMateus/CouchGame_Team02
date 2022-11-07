04c076fb46c1775de25f1130efb625df848956af
Antoine Vine <antoine.vine78@gmail.com>
2022-11-07 15:17:46 +0100

    Audio upgrade

--- Assets/Audio/AudioManager.cs
+++ Assets/Audio/AudioManager.cs
@@ -3,9 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
-public class AudioManage : MonoBehaviour
+public class AudioManager : MonoBehaviour
 {
-    
+
+    public AudioManager instance { get; set; }
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private AudioClip clip;
+   
 
 
+    private void Awake()
+    {
+        instance = this;
+        PlayVFXAudio(clip);
+    }
+
+
+    public void PlayVFXAudio(AudioClip clip)
+    {
+        audioSource.clip = clip;
+        audioSource.Play();
+
+    }
 }
