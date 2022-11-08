5f74ea0fc2df84de5fc0d5400c615d50be7289fe
AlexandreSanchesMateus <90258109+AlexandreSanchesMateus@users.noreply.github.com>
2022-11-08 13:56:28 +0100

    audio manager update

--- Assets/Audio/AudioManager.cs
+++ Assets/Audio/AudioManager.cs
@@ -5,25 +5,48 @@ using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
-
     public AudioManager instance { get; set; }
-    [SerializeField] private AudioSource audioSource;
-    [SerializeField] private AudioMixer audioMixer;
-    [SerializeField] private AudioClip clip;
-   
 
+    [SerializeField]
+    private AudioMixer audioMixer;
+
+    private List<AudioSource> speakers = new List<AudioSource>();
+    [SerializeField] private AudioSource playerAudioSource;
+
+    [SerializeField] AudioClip clip;
 
     private void Awake()
     {
         instance = this;
-        PlayVFXAudio(clip);
     }
 
-
-    public void PlayVFXAudio(AudioClip clip)
+    private void Start()
     {
-        audioSource.clip = clip;
-        audioSource.Play();
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            speakers.Add(this.transform.GetChild(i).GetComponent<AudioSource>());
+        }
+    }
 
+    public void PlayAudioOnSpeaker(AudioClip clip)
+    {
+        foreach(AudioSource source in speakers)
+        {
+            source.PlayOneShot(clip);
+        }
+    }
+
+    public void PlayAudioOnPlayer(AudioClip clip)
+    {
+        playerAudioSource.PlayOneShot(clip);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(UnityEngine.KeyCode.A))
+        {
+            Debug.Log("je suis tout puissant !!!!!!!!!!!!!!!!");
+            PlayAudioOnSpeaker(clip);
+        }
     }
 }
