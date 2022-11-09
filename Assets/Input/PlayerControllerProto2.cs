7f4f0bc17289449c242e31cbd1acb0227af21531
AlexandreSanchesMateus <90258109+AlexandreSanchesMateus@users.noreply.github.com>
2022-11-09 14:43:10 +0100

    Add Headbob

--- Assets/Input/PlayerControllerProto2.cs
+++ Assets/Input/PlayerControllerProto2.cs
@@ -13,14 +13,23 @@ public class PlayerControllerProto2 : MonoBehaviour
     [SerializeField] private float jumpHeight = 1.0f;
     [SerializeField] private float gravityValue = -9.81f;
     [SerializeField] private float sensitivity = 30f;
+
     [Header("Interact Option")]
     [SerializeField] private float radius;
     [SerializeField] private float range;
     [SerializeField] private LayerMask layer;
 
-    private float xRotation;
+    [Header("Headbob Option")]
+    [SerializeField] bool enableHeadbob;
+    [SerializeField, Range(0, 0.1f)] private float _amplitude = 0.015f;
+    [SerializeField, Range(0, 30)] private float _frequency = 10.0f;
+    private float startPosY;
+    private float timer;
+
 
     private CharacterController controller;
+    private float xRotation;
+
     private Vector3 playerVelocity;
     private bool groundedPlayer;
     public static bool enablePlayerMovement { get; set; }
@@ -35,12 +44,16 @@ public class PlayerControllerProto2 : MonoBehaviour
     private void Start()
     {
         controller = gameObject.GetComponent<CharacterController>();
+        startPosY = cameraObj.transform.localPosition.y;
         Cursor.lockState = CursorLockMode.Locked;
         enablePlayerMovement = true;
     }
 
     void Update()
     {
+        if (enableHeadbob)
+            HeadbobHandle();
+
         // Désactivation des mouvement
         if (!enablePlayerMovement)
             return;
@@ -74,6 +87,8 @@ public class PlayerControllerProto2 : MonoBehaviour
 
         Vector3 move = transform.rotation * new Vector3(movementInput.x, 0, movementInput.y);
         controller.Move(move * Time.deltaTime * playerSpeed);
+        
+        
 
         // Souris Horitale
         transform.Rotate(Vector3.up * (rotateInput.x * sensitivity * Time.deltaTime));
@@ -91,6 +106,33 @@ public class PlayerControllerProto2 : MonoBehaviour
 
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
+    }
+
+    private void HeadbobHandle()
+    {
+        if (!controller.isGrounded)
+        {
+            ResetPosition();
+            return;
+        }
+
+        if (Mathf.Abs(movementInput.x) > 0.02f || Mathf.Abs(movementInput.y) > 0.02f)
+        {
+            Debug.Log(movementInput.x + movementInput.y);
+            timer += Time.deltaTime * _frequency;
+            cameraObj.transform.localPosition = new Vector3(cameraObj.transform.localPosition.x, startPosY + Mathf.Sin(timer) * _amplitude, cameraObj.transform.localPosition.z);
+        }
+        else if (cameraObj.transform.localPosition.y != startPosY)
+            ResetPosition();
+    }
+
+    private void ResetPosition()
+    {
+        if (cameraObj.transform.localPosition.y == startPosY)
+            return;
+
+        cameraObj.transform.localPosition = new Vector3(cameraObj.transform.localPosition.x, Mathf.Lerp(cameraObj.transform.localPosition.y, startPosY, Time.time * 0.01f), cameraObj.transform.localPosition.z);
+        timer = 0;
     }
 
     public void OnMove(InputAction.CallbackContext context)
