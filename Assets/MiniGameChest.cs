82bf9716987861db2fc3a872a8e813db986c2197
Thoma-fr <78789215+Thoma-fr@users.noreply.github.com>
2022-11-30 13:29:29 +0100

    oui

--- Assets/MiniGameChest.cs
+++ Assets/MiniGameChest.cs
@@ -9,9 +9,11 @@ using System.Diagnostics.Tracing;
 public class MiniGameChest : MonoBehaviour, IMinigame 
 {
     [SerializeField] private string code;
+    private bool isWin;
     public TextMeshPro text; 
     public bool interact(InputAction.CallbackContext callback)
     {
+        if (!isWin)
             switch (callback.action.name)
             {
                 case "West":
@@ -21,19 +23,30 @@ public class MiniGameChest : MonoBehaviour, IMinigame
                 text.text += ".";
                 break;
                 case "East":
-                text.text += "_";
+                text.text += "-";
                 break;
                 case "North":
                     if(text.text.Length>0)
-                    text.text.Remove(text.text.Length);
+                    text.text = text.text.Substring(0, text.text.Length - 1);
                 break;
                 default:
                 break;
             }
-        
+
         if (text.text == code)
+        {
+            isWin = true;
             return true;
+        }
         return false;
+    }
+    private void Update()
+    {
+        if(isWin)
+        {
+            text.text = "Douglas";
+            text.color = Color.green;
+        }
     }
     public void Move(InputAction.CallbackContext callback) { }
 }
