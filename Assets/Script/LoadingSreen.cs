ddbe71a0a6cbd7997b3ca6dd2971b36394ccf948
Thoma-fr <78789215+Thoma-fr@users.noreply.github.com>
2022-12-01 10:27:03 +0100

    loading screen

--- Assets/Script/LoadingSreen.cs
+++ Assets/Script/LoadingSreen.cs
@@ -1,6 +1,4 @@
 using DG.Tweening;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using UnityEngine.InputSystem;
@@ -9,17 +7,26 @@ public class LoadingSreen : MonoBehaviour, IMinigame
 {
     public Transform pos1, pos2;
     private bool canInteract;
+    [SerializeField] private AudioClip clip1, clip2;
+    private AudioSource audioSource;
+    private int numberRequired;
+    private int currentNumber;
     private void OnEnable()
     {
+        audioSource = GetComponent<AudioSource>();
         Sequence mySequence = DOTween.Sequence();
         mySequence.Append(transform.DOMove(pos2.position,1f));
         mySequence.Append(transform.DOMove(pos1.position,1f));
         mySequence.SetLoops(-1, LoopType.Yoyo);
-        
     }
     public bool interact(InputAction.CallbackContext callback)
     {
-        if (canInteract&& callback.action.name=="West") 
+        if (canInteract && callback.action.name == "West" && callback.started)
+        {
+            audioSource.PlayOneShot(clip1);
+            currentNumber++;
+        }
+        if (currentNumber >= numberRequired)
             return true;
         else
             return false;
@@ -27,19 +34,16 @@ public class LoadingSreen : MonoBehaviour, IMinigame
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("in");
         if (other.CompareTag("LoadingTrigger"))
         {
-           
+            audioSource.PlayOneShot(clip2);
             canInteract = true;
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        Debug.Log("out");
         if (other.CompareTag("LoadingTrigger"))
         {
-            
             canInteract = false;
         }
     }
